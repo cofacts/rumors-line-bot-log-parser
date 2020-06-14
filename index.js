a70a9904b85750c806f7bc8b7a577a5db14c36d0
ggm <godgunman@gmail.com>
2020-06-14 23:42:00 +0800

    Create new Trasfrom instead of singleton

--- index.js
+++ index.js
@@ -9,136 +9,143 @@ const glob = require('glob');
 const HEROKU_MODE = process.env.HEROKU || false;
 const USER_ID = process.env.USER_ID || false;
 
-const filePathTofileNameLines = new Transform({
-  objectMode: true,
-  transform(fileName, encoding, callback) {
-    const rl = readline.createInterface({
-      input: fs.createReadStream(fileName),
-      crlfDelay: Infinity,
-    });
+const filePathTofileNameLines = () =>
+  new Transform({
+    objectMode: true,
+    transform(fileName, encoding, callback) {
+      const rl = readline.createInterface({
+        input: fs.createReadStream(fileName),
+        crlfDelay: Infinity,
+      });
 
-    rl.on('line', line => {
-      this.push({ fileName, line });
-    });
+      rl.on('line', line => {
+        this.push({ fileName, line });
+      });
 
-    rl.on('close', () => callback());
-  },
-});
+      rl.on('close', () => callback());
+    },
+  });
 
-const filenameLineToLineTimestamp = new Transform({
-  objectMode: true,
-  transform({ line, fileName }, encoding, callback) {
-    if (!HEROKU_MODE) {
-      // line on S3:
-      // Pure content, no prefix.
-      // file directory structure: 201709/01/05/0308.70025261277880.log
-      const [, year, month, day, hour, min, second] = fileName.match(
-        /(\d{4})(\d{2})\/(\d{2})\/(\d{2})\/(\d{2})(\d{2})\.\d+\.log$/
+const filenameLineToLineTimestamp = () =>
+  new Transform({
+    objectMode: true,
+    transform({ line, fileName }, encoding, callback) {
+      if (!HEROKU_MODE) {
+        // line on S3:
+        // Pure content, no prefix.
+        // file directory structure: 201709/01/05/0308.70025261277880.log
+        const [, year, month, day, hour, min, second] = fileName.match(
+          /(\d{4})(\d{2})\/(\d{2})\/(\d{2})\/(\d{2})(\d{2})\.\d+\.log$/
+        );
+
+        this.push({
+          timestamp: `${year}-${month}-${day}T${hour}:${min}:${second}Z`,
+          line,
+        });
+        return callback();
+      }
+
+      // line on Heroku:
+      // 112 <190>1 2018-07-16T17:32:16.082803+00:00 app web.1 - - ||LOG||<----------
+      if (!line.includes('||LOG||')) return callback();
+
+      const [, timestamp, content] = line.match(
+        /.*(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}\+\d{2}:\d{2}) .+ \|\|LOG\|\|(.*)/
       );
 
-      this.push({
-        timestamp: `${year}-${month}-${day}T${hour}:${min}:${second}Z`,
-        line,
-      });
-      return callback();
-    }
+      this.push({ line: content, timestamp });
+      callback();
+    },
+  });
 
-    // line on Heroku:
-    // 112 <190>1 2018-07-16T17:32:16.082803+00:00 app web.1 - - ||LOG||<----------
-    if (!line.includes('||LOG||')) return callback();
+const lineTimestampToConversationObj = () =>
+  new Transform({
+    objectMode: true,
+    transform({ line, timestamp }, encoding, callback) {
+      if (line === '<----------') {
+        if (this._buffer !== '') {
+          console.info(
+            `[INFO] Incomplete message detected at ${timestamp}; ignoring`
+          );
+        }
+        this._buffer = '';
 
-    const [, timestamp, content] = line.match(
-      /.*(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}\+\d{2}:\d{2}) .+ \|\|LOG\|\|(.*)/
-    );
-
-    this.push({ line: content, timestamp });
-    callback();
-  },
-});
-
-const lineTimestampToConversationObj = new Transform({
-  objectMode: true,
-  transform({ line, timestamp }, encoding, callback) {
-    if (line === '<----------') {
-      if (this._buffer !== '') {
-        console.info(
-          `[INFO] Incomplete message detected at ${timestamp}; ignoring`
-        );
+        return callback();
       }
-      this._buffer = '';
 
-      return callback();
-    }
+      if (line === '---------->') {
+        let conversationObj;
+        try {
+          conversationObj = JSON.parse(this._buffer);
+        } catch (e) {
+          console.error(
+            `[ERROR] Cannot parse message at ${timestamp}; skipping`
+          );
+          this._buffer = '';
+          return callback();
+        }
 
-    if (line === '---------->') {
-      let conversationObj;
-      try {
-        conversationObj = JSON.parse(this._buffer);
-      } catch (e) {
-        console.error(`[ERROR] Cannot parse message at ${timestamp}; skipping`);
+        const { CONTEXT, INPUT, OUTPUT } = conversationObj;
+        const issuedAt = new Date(CONTEXT.issuedAt);
+        try {
+          this.push({
+            timestamp: new Date(timestamp).toISOString(),
+            userId: USER_ID ? INPUT.userId : undefined,
+            userIdsha256: sha256(INPUT.userId),
+            'input.message.text': collapseLines(
+              INPUT.message && INPUT.message.text
+            ),
+            'context.issuedAt': isNaN(+issuedAt) ? '' : issuedAt.toISOString(),
+            'context.data.searchedText': collapseLines(
+              CONTEXT.data && CONTEXT.data.searchedText
+            ),
+            'context.state': CONTEXT.state,
+            'context.data.selectedArticleId':
+              CONTEXT.data && CONTEXT.data.selectedArticleId,
+            'context.data.selectedReplyId':
+              CONTEXT.data && CONTEXT.data.selectedReplyId,
+            'output.context.state': OUTPUT.context.state,
+            'output.context.data.selectedArticleId':
+              OUTPUT.context.data.selectedArticleId,
+            'output.context.data.selectedReplyId':
+              OUTPUT.context.data.selectedReplyId,
+            'output.replies': collapseLines(
+              (OUTPUT.replies || [])
+                .map(({ text, altText }) => text || altText)
+                .join('↵')
+            ),
+          });
+        } catch (e) {
+          console.error(`[ERROR] message at ${timestamp} contains error`, e);
+        }
+
         this._buffer = '';
         return callback();
       }
 
-      const { CONTEXT, INPUT, OUTPUT } = conversationObj;
-      const issuedAt = new Date(CONTEXT.issuedAt);
-      try {
-        this.push({
-          timestamp: new Date(timestamp).toISOString(),
-          userId: USER_ID ? INPUT.userId : undefined,
-          userIdsha256: sha256(INPUT.userId),
-          'input.message.text': collapseLines(
-            INPUT.message && INPUT.message.text
-          ),
-          'context.issuedAt': isNaN(+issuedAt) ? '' : issuedAt.toISOString(),
-          'context.data.searchedText': collapseLines(
-            CONTEXT.data && CONTEXT.data.searchedText
-          ),
-          'context.state': CONTEXT.state,
-          'context.data.selectedArticleId':
-            CONTEXT.data && CONTEXT.data.selectedArticleId,
-          'context.data.selectedReplyId':
-            CONTEXT.data && CONTEXT.data.selectedReplyId,
-          'output.context.state': OUTPUT.context.state,
-          'output.context.data.selectedArticleId':
-            OUTPUT.context.data.selectedArticleId,
-          'output.context.data.selectedReplyId':
-            OUTPUT.context.data.selectedReplyId,
-          'output.replies': collapseLines(
-            (OUTPUT.replies || [])
-              .map(({ text, altText }) => text || altText)
-              .join('↵')
-          ),
-        });
-      } catch (e) {
-        console.error(`[ERROR] message at ${timestamp} contains error`, e);
-      }
+      this._buffer += line;
+      callback();
+    },
+  });
 
-      this._buffer = '';
-      return callback();
-    }
+const conversationObjFilter = () =>
+  new Transform({
+    objectMode: true,
+    transform(conversationObj, encoding, callback) {
+      // on context.data.searchArticleId change, which means an article is selected
+      // either automatically or manually.
+      //
+      const canOutput =
+        conversationObj['output.context.data.selectedArticleId'] &&
+        conversationObj['context.data.selectedArticleId'] !==
+          conversationObj['output.context.data.selectedArticleId'];
 
-    this._buffer += line;
-    callback();
-  },
-});
+      if (canOutput) this.push(conversationObj);
 
-const conversationObjFilter = new Transform({
-  objectMode: true,
-  transform(conversationObj, encoding, callback) {
-    // on context.data.searchArticleId change, which means an article is selected
-    // either automatically or manually.
-    //
-    const canOutput =
-      conversationObj['output.context.data.selectedArticleId'] &&
-      conversationObj['context.data.selectedArticleId'] !==
-        conversationObj['output.context.data.selectedArticleId'];
+      callback();
+    },
+  });
 
-    if (canOutput) this.push(conversationObj);
-
-    callback();
-  },
-});
 /**
  *
  * @param {string} globFilePath
@@ -152,9 +159,10 @@ const parseToJson = async (globFilePath, callback, options = {}) => {
 
   console.info(`Processing ${inputFiles.length}...`);
 
-  filePathTofileNameLines
-    .pipe(filenameLineToLineTimestamp)
-    .pipe(lineTimestampToConversationObj)
+  const stream = filePathTofileNameLines();
+  stream
+    .pipe(filenameLineToLineTimestamp())
+    .pipe(lineTimestampToConversationObj())
     .pipe(
       new Transform({
         objectMode: true,
@@ -174,10 +182,10 @@ const parseToJson = async (globFilePath, callback, options = {}) => {
    */
 
   const promises = [];
+
   for (let file of inputFiles) {
-    const p = new Promise(resolve =>
-      filePathTofileNameLines.write(file, resolve)
-    );
+    const p = new Promise(resolve => stream.write(file, resolve));
+
     if (sequential) {
       await p;
     } else {
@@ -225,10 +233,11 @@ if (require.main === module) {
   // eslint-disable-next-line no-console
   console.log(`Processing ${inputFiles.length} files to ${outputFilePath}...`);
 
-  filePathTofileNameLines
-    .pipe(filenameLineToLineTimestamp)
-    .pipe(lineTimestampToConversationObj)
-    .pipe(conversationObjFilter)
+  const stream = filePathTofileNameLines();
+  stream
+    .pipe(filenameLineToLineTimestamp())
+    .pipe(lineTimestampToConversationObj())
+    .pipe(conversationObjFilter())
     .pipe(
       csvStringify({
         header: true,
@@ -249,6 +258,6 @@ if (require.main === module) {
     .pipe(fs.createWriteStream(outputFilePath));
 
   inputFiles.forEach(filePath => {
-    filePathTofileNameLines.write(filePath);
+    stream.write(filePath);
   });
 }
