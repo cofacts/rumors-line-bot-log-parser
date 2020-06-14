b0850d7ba8cb65813ddb21d417f7d706026825bc
ggm <godgunman@gmail.com>
2020-06-14 20:21:00 +0800

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 8,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "prettier",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
   ],
-  plugins: [
-    "prettier",
-  ],
+  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ["error", {
-      "trailingComma": "es5",
-      "singleQuote": true,
-    }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
     'no-console': ['error', { allow: ['info', 'error'] }], // no console.log, but can use .info and .error.
   },
   env: {
     node: true,
-  }
+    es6: true,
+  },
 };
